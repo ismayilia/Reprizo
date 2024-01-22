19589badb41e895dde60e172f222d0bc8a075255
HP <[ismayilia@code.edu.az]>
2024-01-22 14:07:04 +0400

    added account to admin panel

---
+++ Reprizo/Reprizo/Areas/Admin/ViewModels/Account/UserRoleVM.cs
@@ -0,0 +1,8 @@
+﻿namespace Reprizo.Areas.Admin.ViewModels.Account
+{
+    public class UserRoleVM
+    {
+        public string UserId { get; set; }
+        public string RoleId { get; set; }
+    }
+}
---
+++ Reprizo/Reprizo/Areas/Admin/ViewModels/Account/UserVM.cs
@@ -0,0 +1,11 @@
+﻿namespace Reprizo.Areas.Admin.ViewModels.Account
+{
+    public class UserVM
+    {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public IList<string> RoleName { get; set; }
+    }
+}
